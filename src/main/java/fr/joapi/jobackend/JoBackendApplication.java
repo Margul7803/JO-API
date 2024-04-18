29942bdc40a80e1852c3e6620d33d52e8d5c0da7
mario <mario.aboujamra@weblib.eu>
2024-04-18 09:48:35 +0200

    🎉: init JO-API project

---
+++ src/main/java/fr/joapi/jobackend/JoBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.joapi.jobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/joapi/jobackend/JoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.joapi.jobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
